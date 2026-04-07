db60ee767cf9c6e579a70bc96c89073c7802842d
DELL <DELL@DELL-PC>
2026-04-07 21:27:13 +0530

    end of section 7

---
+++ Client/src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import 'zone.js';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideZoneChangeDetection()
+  ]
+};
